17e3347280921d3b61330fcc2d19ef8fd50471c6
mubarak salisu <marksmanl619@gmail.com>
2026-04-15 09:09:17 +0100

    Chores(express): setup express

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ message: "API is ready 🚀" });
+});
+
+export default app;
