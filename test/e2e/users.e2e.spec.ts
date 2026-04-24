0dd4c2c6d8d317f680b8fe73809e8c3370402c4f
mubarak salisu <marksmanl619@gmail.com>
2026-04-24 08:28:58 +0100

    Test(POST /api/users/register): write test for register endpoint of users route

--- test/e2e/users.e2e.spec.ts
+++ test/e2e/users.e2e.spec.ts
@@ -25,6 +25,32 @@ describe("/api/users", () => {
       const res = await request(app).post("/api/users/register").send(userData);
 
       expect(res.statusCode).toEqual(201);
+      expect(res.body).toHaveProperty("id");
+      expect(res.body).toHaveProperty("firstName", userData.firstName);
+      expect(res.body).toHaveProperty("lastName", userData.lastName);
+      expect(res.body).toHaveProperty("email", userData.email);
+      expect(res.body).toHaveProperty("phoneNumber", userData.phoneNumber);
+      expect(res.body).not.toHaveProperty("password");
+    });
+
+    it("should not create a user with existing email", async () => {
+      await request(app).post("/api/users/register").send(userData);
+      const res = await request(app).post("/api/users/register").send(userData);
+
+      expect(res.statusCode).toEqual(400);
+      expect(res.body.message).toEqual("User with this email already exists");
+    });
+
+    it("should not create a user with existing phone number", async () => {
+      await request(app).post("/api/users/register").send(userData);
+      const res = await request(app)
+        .post("/api/users/register")
+        .send({ ...userData, email: "different@example.com" });
+
+      expect(res.statusCode).toEqual(400);
+      expect(res.body.message).toEqual(
+        "User with this phone number already exists",
+      );
     });
   });
 });
