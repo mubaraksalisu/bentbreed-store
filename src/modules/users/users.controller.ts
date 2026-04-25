369d727880709641814f10e9883769e1731c0ed0
mubarak salisu <marksmanl619@gmail.com>
2026-04-25 07:41:28 +0100

    Refactor(modules.users.controller): get rid of try-catch from users controller, express handles it automatically

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,54 +1,34 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import UsersService from "./users.service";
 
 export default class UsersController {
   constructor(private usersService: UsersService) {}
 
-  register = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const user = await this.usersService.create(req.body);
-      res.status(201).json(user);
-    } catch (err) {
-      next(err);
-    }
+  register = async (req: Request, res: Response) => {
+    const user = await this.usersService.create(req.body);
+    res.status(201).json(user);
   };
 
-  findById = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const user = await this.usersService.findById(req.params.id as string);
-      res.status(200).json(user);
-    } catch (err) {
-      next(err);
-    }
+  findById = async (req: Request, res: Response) => {
+    const user = await this.usersService.findById(req.params.id as string);
+    res.status(200).json(user);
   };
 
-  find = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const users = await this.usersService.find();
-      res.status(200).json(users);
-    } catch (err) {
-      next(err);
-    }
+  find = async (req: Request, res: Response) => {
+    const users = await this.usersService.find();
+    res.status(200).json(users);
   };
 
-  update = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const user = await this.usersService.update(
-        req.params.id as string,
-        req.body,
-      );
-      res.status(200).json(user);
-    } catch (err) {
-      next(err);
-    }
+  update = async (req: Request, res: Response) => {
+    const user = await this.usersService.update(
+      req.params.id as string,
+      req.body,
+    );
+    res.status(200).json(user);
   };
 
-  remove = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await this.usersService.remove(req.params.id as string);
-      res.status(204);
-    } catch (err) {
-      next(err);
-    }
+  remove = async (req: Request, res: Response) => {
+    await this.usersService.remove(req.params.id as string);
+    res.status(204);
   };
 }
