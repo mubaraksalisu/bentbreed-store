10998fa5881bfc207be5b3175e719ab2f809a893
mubarak salisu <marksmanl619@gmail.com>
2026-05-01 10:57:34 +0100

    Feature(POST /api/auth/login): implement the login route of auth module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -10,4 +10,10 @@ export default class AuthController extends BaseController {
     const user = await this.authService.registerUser(req.body);
     this.created(res, user, "User registered successfully");
   };
+
+  login = async (req: any, res: any) => {
+    const { email, password } = req.body;
+    const tokens = await this.authService.loginUser(email, password);
+    this.ok(res, tokens, "User logged in successfully");
+  };
 }
--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -6,6 +6,7 @@ import UsersService from "../users/users.service";
 import UserRepository from "../users/users.repository";
 import { validateReqBody } from "../../common/middleware/validate-req-body.middleware";
 import { registerSchema } from "../users/users.validation";
+import { loginSchema } from "./auth.validation";
 
 const router = express.Router();
 
@@ -14,8 +15,9 @@ const usersService = new UsersService(new UserRepository());
 const authService = new AuthService(usersService, authRepository);
 const authController = new AuthController(authService);
 
-const { register } = authController;
+const { register, login } = authController;
 
 router.post("/register", validateReqBody(registerSchema), register);
+router.post("/login", validateReqBody(loginSchema), login);
 
 export default router;
